45b5bf6fcbc4452b0f4a31ed2c099e56a623a8b4
Durgesh Bhatt <durgeshbhatt360@gmail.com>
2026-09-02 23:59:59 +0530

    deployment changes

--- src/queue/redisClient.ts
+++ src/queue/redisClient.ts
@@ -1,6 +1,6 @@
-import { Redis } from "ioredis";
-import "../config/config.ts";
-
-export const redis = new Redis(
-  process.env.REDIS_URL!
-);
+   import {Redis} from "ioredis";
+   import '../config/config.ts'
+   
+   export const redis = process.env.REDIS_URL
+     ? new Redis(process.env.REDIS_URL, { tls: {} })   // Upstash requires TLS
+     : new Redis({ host: "localhost", port: 6379 });
